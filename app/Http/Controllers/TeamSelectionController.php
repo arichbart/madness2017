a86ee2aa27ffd73d3058c15fb100caaf4646d5ad
AARON RICHBART <aaronrichbart@AARONs-MacBook-Pro-2.local>
2021-03-15 22:00:49 -0400

    changes for 2021

--- app/Http/Controllers/TeamSelectionController.php
+++ app/Http/Controllers/TeamSelectionController.php
@@ -30,7 +30,7 @@ class TeamSelectionController extends Controller
 
     public function postTeams() {
     	$now = carbon::now();
-    	if ($now > "2019-03-21 16:15:00.000000") {
+    	if ($now > "2021-03-21 16:15:00.000000") {
     		return back()
 				->withErrors('You are too late.  Games have already started')
 				->withInput();
